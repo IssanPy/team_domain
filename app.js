1da10c2432206dcca90bf88ed2b4d1d9aabad645
Parth Sinh <107989591+iamalsauser@users.noreply.github.com>
2024-03-01 01:11:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import OurTeamPage from './components/OurTeamPage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <OurTeamPage />
+    </div>
+  );
+}
+
+export default App;
+
